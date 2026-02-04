7b07e6ced84e532610fe2242a7d18289d6680095
carminenicotera <carmine.91.n@hotmail.it>
2026-02-04 16:47:47 +0100

    complete fourth task arrays bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -36,7 +36,14 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = [false];
+
+for (let i = 0; i < teachers.length; i++) {
+  if(teachers[i] === 'Fabio') {
+    isFabioPresent[0] = true;
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
