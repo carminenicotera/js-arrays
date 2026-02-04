7427682b5bd1aaccc914a10eaa8929d6bf2db273
carminenicotera <carmine.91.n@hotmail.it>
2026-02-04 14:56:33 +0100

    complete third task arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -19,7 +19,9 @@ console.log(teachers[4]);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers[6];
+teachers.pop();
+console.log(`${lastTeacher} ${teachers}`);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
